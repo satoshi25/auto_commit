efd607bee7503a1063bc9b50639c0fcddcfcffad
satoshi25 <satoshi25.kr@gmail.com>
2024-01-08 19:22:50 +0900

    feat: utils 모듈에 openFile 함수 추가 - 해당 파일들의 데이터를 가져오는 openFile 함수 추가

--- src/index.js
+++ src/index.js
@@ -5,10 +5,18 @@ const [year, month, day] = utils.getTime();
 
 // 참조하는 폴더 내부에 날짜에 맞는 파일들을 찾는다.
 const foundFile = utils.findFiles(month, day);
-console.log(foundFile);
 
 // 파일이 없다면 진행하지 않는다.
-// 파일들이 있다면 다음으로 진행한다.
-// 해당 파일들을 읽어 문서들과, message를 가져온다.
+if (foundFile.length === 0) {
+	console.log('오늘 날짜에 맞는 파일이 존재하지 않습니다.');
+	console.log(`${year}년 ${month}월 ${day}일의 auto_commit은 동작하지 않습니다.`);
+} else {
+	// 파일들이 있다면 다음으로 진행한다.
+
+	// 해당 파일들을 읽어 문서들과, message를 가져온다.
+	let dataObj = utils.openFile(foundFile);
+	console.log(dataObj);
+}
+
 // 해당 문서들 정보로 commit 할 폴더의 유무를 확인한 후 commit할 위치에 파일을 생성한다.
 // sh 파일을 실행하여 commit 한다.
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import process from 'process';
 
+
+const cur = process.cwd();
+const uploadFolder = path.join(cur, 'public', 'commit_files');
+
+
 export function getTime() {
 	const time = new Date();
 	const year = time.getFullYear();
@@ -16,9 +21,8 @@ export function getTime() {
 	return [year, month, day];
 }
 
+
 export function findFiles(month, day) {
-	const cur = process.cwd();
-	const uploadFolder = path.join(cur, 'public', 'commit_files');
 	const files = fs.readdirSync(uploadFolder);
 	let findFile = files.filter((file) => {
 		const name = file.split('.')[0];
@@ -29,4 +33,25 @@ export function findFiles(month, day) {
 	return findFile.map((file) => {
 		return file.split('.');
 	});
+}
+
+
+export function openFile(files) {
+	let dataObj = {};
+	for (let i = 0; i < files.length; i++) {
+		const data = fs.readFileSync(`${uploadFolder}/${files[i][0]}.${files[i][1]}`).toString();
+		if (files[i][1] === 'py' || files[i][1] === 'js') {
+			let lineData = data.split('\n');
+			if (lineData.length > 3) {
+				dataObj["message"] = lineData[3];
+			} else {
+				dataObj["message"] = undefined;
+			}
+			dataObj["question"] = data;
+			dataObj["language"] = files[i][1];
+		} else {
+			dataObj["input"] = data;
+		}
+	}
+	return dataObj;
 }
